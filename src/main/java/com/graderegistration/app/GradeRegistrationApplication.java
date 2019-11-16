b69d1f5ef63779055a3e68b758ee235ce0bb53c6
arondelooijer <aron@mbbma.nl>
2019-11-16 11:40:22 +0100

    Initial commit

---
+++ src/main/java/com/graderegistration/app/GradeRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.graderegistration.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradeRegistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/graderegistration/app/GradeRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graderegistration.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradeRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
